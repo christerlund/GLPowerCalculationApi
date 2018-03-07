4c12919718ddd87029406f8d1e5ee02a0b9b354a
christerlund <36890996+christerlund@users.noreply.github.com>
2018-03-07 17:49:11 +0100

    Add files via upload

---
+++ GLPowerCalculationApi/GLPowerCalculationApi/Controllers/PowerController.cs
@@ -0,0 +1,43 @@
+﻿using GLPowerCalculationApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GLPowerCalculationApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PowerController : Controller
+    {
+        /// <summary>
+        /// Simple method for calculating : "Strøm på I2 ved symmetrisk last i 3 fas system"
+        /// </summary>
+        /// <param name="item"> </param>
+        /// <returns> Calculated value for I2</returns>
+
+        // POST api/power
+        [HttpPost]
+        public string Post([FromBody]CalculationItem item)
+        {
+            double I2 = 0;
+       
+            try
+            {
+                if (!ModelState.IsValid) { return "Model is invalid"; }
+
+                I2 = CalculatePower(item);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+   
+                  
+            return string.Format("{{\"I2\":{0}}}", I2.ToString()); ;
+        }
+
+        private static double CalculatePower(CalculationItem item)
+        {
+            return (((item.P * Math.Sqrt(3)) - (item.U1 * item.I1) - (item.U3 * item.I3)) / item.U2);
+        }
+
+    }
+}
---
+++ GLPowerCalculationApi/GLPowerCalculationApi/Models/CalculationItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GLPowerCalculationApi.Models
+{
+
+    /// <summary>
+    /// Model for holding calculation data
+    /// 
+    /// Regex - "^(0|[1-9][0-9]*)$" allows 0 and positive whole numbers  
+    /// </summary>
+    public class CalculationItem
+    {
+        [RegularExpression("^(0|[1-9][0-9]*)$")]
+        public int P { get; set; }
+        [RegularExpression("^(0|[1-9][0-9]*)$")]
+        public int U1 { get; set; }
+        [RegularExpression("^(0|[1-9][0-9]*)$")]
+        public int U2 { get; set; }
+        [RegularExpression("^(0|[1-9][0-9]*)$")]
+        public int U3 { get; set; }
+        [RegularExpression("^(0|[1-9][0-9]*)$")]
+        public int I1 { get; set; }
+        [RegularExpression("^(0|[1-9][0-9]*)$")]
+        public int I3 { get; set; }
+    }
+}
